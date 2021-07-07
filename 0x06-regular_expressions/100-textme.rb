577182b040c7b746ee9e6bc087b648b613762168
JuanDAC <juanavila12.0308@gmail.com>
2021-07-06 20:45:48 -0400

    [fix] (join within '','')

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(?<SENDER>.*?)\] .*\[to:(?<RECEIVER>.*?)\] .*\[flags:(?<FLAGS>.*?)\].*/).join
+puts ARGV[0].scan(/\[from:(?<SENDER>.*?)\] .*\[to:(?<RECEIVER>.*?)\] .*\[flags:(?<FLAGS>.*?)\].*/).join(",")
