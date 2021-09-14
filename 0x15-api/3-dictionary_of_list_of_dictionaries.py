e210f2b7f82c0d529222156c5819b52be84276c0
JuanDAC <juanavila12.0308@gmail.com>
2021-09-13 23:19:02 -0400

    [style] (add style of features)

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -17,21 +17,21 @@ domine = 'https://jsonplaceholder.typicode.com'
 
 
 if __name__ == "__main__":
-
     users = getjson(domine, 'users')
     todos = getjson(domine, 'todos')
-
     users_2_dict = {}
     i = 0
     for user in users:
         id_user = user.get('id')
         users_2_dict[id_user] = []
-        user_tacks = [{"task": task.get('title'),
-                      "completed": task.get('completed'),
-                      "username": user.get("username")}
-                     for task in todos if id_user == task.get("userId")]
+        user_tacks = [
+                {
+                    "task": task.get('title'),
+                    "completed": task.get('completed'),
+                    "username": user.get("username")
+                }
+                for task in todos if id_user == task.get("userId")]
         users_2_dict[id_user] = user_tacks
-
 
     with open('todo_all_employees.json', "w", newline="") as file:
         json.dump(users_2_dict, file)
